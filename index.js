6331b41bf7f5a77ba046824891a6bc269a789a4f
Shaheer Alam <134544657+shaheer-alam-bit@users.noreply.github.com>
2025-04-05 16:52:37 +0500

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ app.use(express.static(__dirname + '/public'));
 
 // Route to handle the default page
 app.get('/', function(request, response) {
-  response.send('Hello World this is from shaheer branch!');
+  response.send('Bye Bye');
 });
 
 // Listen on all interfaces (0.0.0.0), not just localhost
